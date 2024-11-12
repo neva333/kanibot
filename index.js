771592eb8b2f9b9fb02ecb19a98bd10ce3a7126c
Your Name <omoitukanai1225l@example.com>
2024-11-12 23:06:27 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+const path = require('path');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async message => {
+    if (message.content === '/kani') {
+        const channel = message.member.voice.channel;
+        if (channel) {
+            const connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
+            });
+
+            const player = createAudioPlayer();
+            const resource = createAudioResource(path.join(__dirname, 'a.wav'));
+
+            player.play(resource);
+            connection.subscribe(player);
+
+            player.on(AudioPlayerStatus.Idle, () => {
+                setTimeout(() => {
+                    player.play(createAudioResource(path.join(__dirname, 'a.wav')));
+                }, 5000);
+            });
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
